08d8207769fafb98ccdc2ae53f1b3e3864fd1080
antonio arista <ad.arista@acad.ucb.edu.bo>
2023-03-30 14:27:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const reyesGodos=[
+    {
+      nombre: "Ataúlfo",
+      aficion: "comer toros sin pelar",
+    },{
+      nombre: "Recesvinto",
+      aficion: "leer a Hegel en arameo",
+    },{
+      nombre: "Teodorico",
+      aficion: "la cría del escarabajo en almíbar"
+    }
+  ]
+
+  const [text, setText] = useState('')
+  let [counter, setCounter] = useState(0)
+
+  const changeText = () => {
+    setText(<h1>Al Rey <span className='king'>{reyesGodos[counter].nombre}</span> le gusta <span className='hooby'>{reyesGodos[counter].aficion}</span></h1>)
+    increment()
+  }
+
+  const increment = () => { 
+    counter < reyesGodos.length - 1
+    ?
+    setCounter(++counter)
+    :
+    setCounter(0)
+
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <button onClick={ changeText }>Ver siguiente</button>
+      <div>{ text }</div>
     </div>
-  );
+    
+    </>
+  )
 }
 
 export default App;
